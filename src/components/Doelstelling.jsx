f42240c8b7c1a9146a40e630fe32082e238210c7
Mert Sahin <mert.sahin@student.hogent.be>
2022-04-18 21:52:13 +0200

    grafieken in doelstelling margin toegevoegd

--- src/components/Doelstelling.jsx
+++ src/components/Doelstelling.jsx
@@ -11,7 +11,7 @@ export default function Doelstelling(d) {
            <div className="border-2 border-[#004C69] text-left p-1 m-2">
           <div className="grid grid-cols-2">
             <div>{naam}</div>
-            <div className="justify-self-end">
+            <div className="justify-self-end mt-7">
               <BarChart></BarChart>
             </div>
           </div>
