4be6281d83dbfd43ec796a337b53f822163459e6
Ben <79012763+benartshogent@users.noreply.github.com>
2022-04-20 12:46:20 +0200

    css categorie dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -7,30 +7,53 @@ export default function Dashboard()
   return (
     <>
       <h1 className={styles.title}>Dashboard</h1>
-      {/* TODO categorie container */}
-      <div className={styles.categorie}>
-        <div className={styles.categorie_heading}>
-          <h2 className={styles.title_categorie}>
-            Economie
-          </h2>
-          <div className={styles.sdg_container}>
-            <img className={styles.sdg} src={sdg1} alt="sdg1"/>
-            <img className={styles.sdg} src={sdg2} alt="sdg2"/>
+      <div className={styles.categorie_container}>
+        <div className={styles.categorie}>
+          <div className={styles.categorie_heading}>
+            <h2 className={styles.title_categorie}>
+              Economie
+            </h2>
+            <div className={styles.sdg_container}>
+              <img className={styles.sdg} src={sdg1} alt="sdg1"/>
+              <img className={styles.sdg} src={sdg2} alt="sdg2"/>
+            </div>
+            <button className={styles.hide_button}>^</button>
           </div>
-          <button className={styles.hide_button}>^</button>
+          <div className={styles.doelstellingen_container}>
+            <div className={styles.doelstelling}>
+              <h3 className={styles.title_doelstelling}>
+                Doelstelling 1
+              </h3>
+            </div>
+            <div className={styles.doelstelling}>
+              <h3 className={styles.title_doelstelling}>
+                Doelstelling 2
+              </h3>
+            </div>
+          </div>
         </div>
-
-        <div className={styles.doelstellingen_container}>
-          <div className={styles.doelstelling}>
-            <h3 className={styles.title_doelstelling}>
-              Doelstelling 1
-            </h3>
+        <div className={styles.categorie}>
+          <div className={styles.categorie_heading}>
+            <h2 className={styles.title_categorie}>
+              Economie
+            </h2>
+            <div className={styles.sdg_container}>
+              <img className={styles.sdg} src={sdg1} alt="sdg1"/>
+              <img className={styles.sdg} src={sdg2} alt="sdg2"/>
+            </div>
+            <button className={styles.hide_button}>^</button>
           </div>
-
-          <div className={styles.doelstelling}>
-            <h3 className={styles.title_doelstelling}>
-              Doelstelling 2
-            </h3>
+          <div className={styles.doelstellingen_container}>
+            <div className={styles.doelstelling}>
+              <h3 className={styles.title_doelstelling}>
+                Doelstelling 1
+              </h3>
+            </div>
+            <div className={styles.doelstelling}>
+              <h3 className={styles.title_doelstelling}>
+                Doelstelling 2
+              </h3>
+            </div>
           </div>
         </div>
       </div>
