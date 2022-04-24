975d798a105b1b075492032f7525a339135d25fe
Mert Sahin <mert.sahin@student.hogent.be>
2022-04-24 22:02:11 +0200

    data ophalen lus opgelost

--- src/components/Doelstelling.jsx
+++ src/components/Doelstelling.jsx
@@ -1,5 +1,8 @@
 import { NavLink } from "react-router-dom";
 import BarChart from "./BarChart";
+import {useData} from '../contexts/DataProvider'
+import { useEffect } from "react";
+
 
 export default function Doelstelling(d) {
   const {naam, id, subdoelstellingen} = d;
