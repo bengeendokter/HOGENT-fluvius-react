8bc2039a1f529dc5c55df602a146234245b1265f
Ben <79012763+benartshogent@users.noreply.github.com>
2022-05-07 22:46:22 +0200

    versimpellen

--- src/components/AccordionCategory.jsx
+++ src/components/AccordionCategory.jsx
@@ -13,44 +13,36 @@ export default function AccordionCategory({naam, id, doelstellingen, sdgs})
 
   return (
     <>
-      <div className="accordion-item bg-white m-4">
-        <div className="accordion-header mb-0
-          text-white text-left" id={`headingOne${id}`}>
-
-          <button className="
-            accordion-button
-            relative
-            flex
-            items-center
-            w-full
-            px-5
-            text-base text-gray-800 text-left
-            rounded-none
-            transition
-            focus:outline-none
-             rounded-none bg-white text-white text-left p-1 grid grid-cols-2 rounded-none
-            w-full
-          
-            " type="button" data-bs-toggle="collapse" data-bs-target={`#collapseOne${id}`} aria-expanded="true"
-            aria-controls={`collapseOne${id}`} >
-
-            <div>
-              <NavLink key={id} to={`/categorieDashboard/${id}`}>
-                <h2 onClick={handleClick} data-cy="categorieClick" className="text-xl hover:underline">{naam}</h2>
-              </NavLink>
-            </div>
-
-            <div className="justify-self-end mr-2">
-              {sdgs.map(sdg => sdg.AFBEELDINGNAAM).filter((sdgNaam, index, array) => array.indexOf(sdgNaam) === index).map(sdgNaam => <img key={sdgNaam} src={`/assets/images/${sdgNaam}.jpg`} alt={`sdg ${sdgNaam}`} className="w-12 inline-block p-1" />)}
-            </div>
+      <div className="accordion-item">
+        <div className="accordion-header" id={`headingOne${id}`}>
+          <button
+          className="accordion-button flex"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target={`#collapseOne${id}`}
+          aria-expanded="true"
+          aria-controls={`collapseOne${id}`} >
           </button>
+            {/* <div className="justify-self-end mr-2">
+              {sdgs.map(sdg => sdg.AFBEELDINGNAAM).filter((sdgNaam, index, array) => array.indexOf(sdgNaam) === index).map(sdgNaam => <img key={sdgNaam} src={`/assets/images/${sdgNaam}.jpg`} alt={`sdg ${sdgNaam}`} className="w-12 inline-block p-1" />)}
+            </div> */}
+              <NavLink key={id} to={`/categorieDashboard/${id}`}>
+                <h2
+                onClick={handleClick}
+                data-cy="categorieClick"
+                className="text-xl hover:underline">{naam}</h2>
+              </NavLink>
         </div>
 
-        <div id={`collapseOne${id}`} className="accordion-collapse collapse show rounded-none" aria-labelledby={`headingOne${id}`}>
-          <div className="accordion-body py-4 px-5 border-2 border-[#004C69]  rounded-none ">
-            <br />
+        <div
+        id={`collapseOne${id}`}
+        className="accordion-collapse collapse show"
+        aria-labelledby={`headingOne${id}`}>
+          <div className="accordion-body">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-              {doelstellingen.sort(({naam: a}, {naam: b}) => a.localeCompare(b)).map(d => <DoelstellingPreview key={d.id} {...d}></DoelstellingPreview>)}
+              {doelstellingen
+              .sort(({naam: a}, {naam: b}) => a.localeCompare(b))
+              .map(d => <DoelstellingPreview key={d.id} {...d}></DoelstellingPreview>)}
             </div>
           </div>
         </div>
