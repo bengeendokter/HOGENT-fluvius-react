3cdf442dd554c2c4c02dd7abfeb9a009bae9ef1f
Mert Sahin <mert.sahin@student.hogent.be>
2022-05-07 16:19:36 +0200

    werkende breadcrumb na refactoring

--- src/contexts/DoelstellingProvider.jsx
+++ src/contexts/DoelstellingProvider.jsx
@@ -161,56 +161,6 @@ import {
 
     }, [doelstellingen]);
 
-    const getSubWithNaam = useCallback(async (ID) => {
-      try {
-        setError('');
-        setLoading(true);
-        for (var m = 0; m < doelstellingen.length; m++) {
-          //top niveau doelstellingen overlopen
-          if (doelstellingen[m].soort === 'COMP') {
-            //kijken of ze subdoelstellingen hebben
-            if (doelstellingen[m].subdoelstellingen && doelstellingen[m].subdoelstellingen.length >= 1) {
-                //iteratie over eerste subs
-                for (var y = 0; y < doelstellingen[m].subdoelstellingen.length; y++) {
-                  //kijk naar de id
-                  const lijst = doelstellingen[m].subdoelstellingen;
-                  if (lijst[y].naam === (ID)) {
-                    //gelijkstellen en break;
-                    return lijst[y];
-                  }
-    
-                  //niet gelijk maar check naar subs
-    
-                  if (lijst[y].soort === 'COMP') {
-                    if (lijst[y].subdoelstellingen && lijst[y].subdoelstellingen.length >= 1) {
-                      for (var z = 0; z < lijst[y].subdoelstellingen.length; z++)  {
-                        const lijst1 = lijst[y].subdoelstellingen;
-                        
-                        if (lijst1[z].naam === (ID)) {
-                          //gelijkstellen en break;
-                          return lijst1[z];
-                        }
-    
-                      }
-                    }
-                  }
-                  
-    
-                }
-            }
-          }
-          //next doelstelling
-        }
- return null;
-      } catch (error) {
-        setError(error);
-        return null;
-      } finally {
-        setLoading(false)
-      }
-
-    }, [doelstellingen]);
-
     const setCurrentDoelstelling = useCallback(async (id) => {
       try {
         setError('');
@@ -281,7 +231,7 @@ import {
     // }), [setCatId, refreshDoelstellingen, getDoelstellingPerRolByID,getDoelstellingByCategorieID, doelstellingen, error, setError, loading, setLoading])
 
     return (
-      <DoelstellingContext.Provider value={{setPad, pad, currentDoel, setCurrentDoelstelling, getSubWithID, getSubWithNaam,  getDoelstellingenVoorCategories, setCategories, categoriesMetDoelstellingen, doelstellingenCat, setCatId, refreshDoelstellingen, getDoelstellingPerRolByID,getDoelstellingByCategorieID, doelstellingen, error, setError, loading, setLoading}}>
+      <DoelstellingContext.Provider value={{setPad, pad, currentDoel, setCurrentDoelstelling, getSubWithID,  getDoelstellingenVoorCategories, setCategories, categoriesMetDoelstellingen, doelstellingenCat, setCatId, refreshDoelstellingen, getDoelstellingPerRolByID,getDoelstellingByCategorieID, doelstellingen, error, setError, loading, setLoading}}>
         {children}
       </DoelstellingContext.Provider>
     );
--- src/pages/DoelstellingDashboard.jsx
+++ src/pages/DoelstellingDashboard.jsx
@@ -1,36 +1,69 @@
 
 import Accordion from '../components/Accordion';
 import { DoelstellingContext} from '../contexts/DoelstellingProvider';
-import {useParams, useLocation } from "react-router-dom";
+import {useParams } from "react-router-dom";
 import {
   useContext, useEffect
 } from 'react';
 import { NavLink } from "react-router-dom";
-import { useCategories } from "../contexts/CategorieProvider";
-
-
 export default function DoelstellingDashboard() {
-  const {doelstellingen, setCurrentDoelstelling, currentDoel, getSubWithID, pad} = useContext(DoelstellingContext);
+  const {doelstellingen, setCurrentDoelstelling, currentDoel,pad} = useContext(DoelstellingContext);
   const { id } = useParams();
-  //const {currentCategorie, setCurrent, categories} = useCategories();
-  //let doelstelling = doelstellingen.filter(e => e.id === Number(id))[0];
-
-  
   useEffect(() => {
     //laden
     if (doelstellingen.length >= 1) {
       setCurrentDoelstelling(id);
     }
     
-    
   }, [doelstellingen, id, setCurrentDoelstelling]);
   
-  return (<>
-   
+  return (
 
-        {currentDoel && pad && console.log(pad)}
+    <>
+    {currentDoel && pad &&
+      <>
+       <div className="m-2 border-2 border-[#004C69]">
+        <div className="border-2 border-[#004C69] bg-[#004C69] text-white text-left p-1 grid grid-cols-2">
+          <div className="ml-2">
+    <NavLink
+			to="/dashboard"
+      className="underline"
+			>
+			Dashboard 
+		</NavLink>
+    &nbsp;  -  &nbsp;
+    <NavLink
+			to={`/categorieDashboard/${(currentDoel.categorie === undefined || currentDoel.categorie.id === null) ? 2: currentDoel.categorie.id}`}
+      className="underline"
+			>
+			 {(currentDoel.categorie === undefined || currentDoel.categorie.naam === null) ? "Ecologie": currentDoel.categorie.naam}
+		</NavLink>
 
-  
+    {
+      pad.map(p=>  { 
+        return <> &nbsp; -  &nbsp;
+      { (p.naam !== currentDoel.naam) ?
+       <NavLink
+         to={`/doelstellingDashboard/${p.id}`}
+         className="underline"
+         >
+          {p.naam}
+       </NavLink>
+      : p.naam}
+      </>
+       })
+    }
+    </div>
+
+    <div className="justify-self-end mr-2">Sdgs</div>
+    </div>
+
+ {<div className="accordion min-w-full px-4">
+          {currentDoel.subdoelstellingen && currentDoel.subdoelstellingen.map(d => <Accordion {...d}></Accordion>)}
+        </div>}
+    </div>
+    </>
+  }
     </>
   );
 }
