253498aa38e8b3b8d2fe2bc532e40231635335fe
Mert Sahin <mert.sahin@student.hogent.be>
2022-05-18 21:20:56 +0200

    data met alle doelwaardes halen voor chart

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,125 +1,88 @@
-import React, {useEffect} from "react";
+import React, {useContext, useEffect} from "react";
 import {Bar, Chart} from "react-chartjs-2";
 import zoomPlugin from "chartjs-plugin-zoom";
 import annotationPlugin from "chartjs-plugin-annotation";
-import {useData} from "../contexts/DataProvider";
+//import {useData, getAllDataByDoelstellingId, alldata} from "../contexts/DataProvider";
 import styles from "./BarChart/BarChart.module.css"
 import {BloodtypeOutlined} from "@mui/icons-material";
+import { DataContext } from '../contexts/DataProvider';
 
 Chart.register(zoomPlugin);
 Chart.register(annotationPlugin);
 
 const BarChart = ({naam, id}) =>
 {
-  const {data: x} = useData();
+  //const {data: x} = useData();
+  const {data: x, getAllDataByDoelstellingId, alldata} = useContext(DataContext);
 
-  if(x.length === 0)
-  {
-    return <></>
-  }
+  useEffect(() => {
+    if(x.length >= 1) {
+      getAllDataByDoelstellingId(id);
+    }
+  }, []);
 
-  const labels = [""];
+  //const labels = [""];
   const dataD = x.filter(d => d.naam === naam);
-  const doelwaardes = dataD[0]['doelwaarde'];
+  //const doelwaardes = dataD[0]['doelwaarde'];
   const eenheid = dataD[0]['eenheid'];
   //const kleuren = ["#055063", "#05635f", "#056348", "#053963", "#0b7ad3"];
   const kleuren = ["#d30b7a", "#d3640b", "#0bd364", "#0bd364", "#640bd3"];
   const gebruikt = [];
 
-  const datas = dataD[0]['data'].map(d =>
-  {
-    //const kleur = `${ '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase()}`;
-    let kleur = kleuren[Math.floor(Math.random()*kleuren.length)];
-    let voorwaarde = gebruikt.includes(kleur);
-    while (voorwaarde) {
-      kleur = kleuren[Math.floor(Math.random()*kleuren.length)];
-      voorwaarde = gebruikt.includes(kleur);
-    }
-    gebruikt.push(kleur);
+  //label n keer opvullen met '' als lege array
+  //dataset = bars en line met array van doelwaarden 
+  let doels = [];
 
-    return {
-      label: `${Object.entries(d)[0][0]}`,
-      data: labels.map(() => Object.entries(d)[0][1][0]),
-      backgroundColor: kleur,
-      hoverBackgroundColor: kleur,
-      borderColor: kleur,
-      borderWidth: 2,
+  //let omgekeerd = alldata.reverse();
+
+  let datas = alldata.reverse().map(d =>
+    {
+      //const kleur = `${ '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase()}`;
+      let kleur = kleuren[Math.floor(Math.random()*kleuren.length)];
+      let voorwaarde = gebruikt.includes(kleur);
+      while (voorwaarde) {
+        kleur = kleuren[Math.floor(Math.random()*kleuren.length)];
+        voorwaarde = gebruikt.includes(kleur);
+      }
+      gebruikt.push(kleur);
+
+      doels.push(d.doelwaarde);
+  
+      return {
+        label: d.jaar,
+        data: d.data[0],
+        backgroundColor: kleur,
+        hoverBackgroundColor: kleur,
+        borderColor: kleur,
+        borderWidth: 2,
+      }
     }
-  }
   );
 
+  datas.push({
+    type: 'line',
+    label: 'Doelwaarden/Drempelwaarden',
+    data: doels,
+    fill: false,
+    borderColor: 'rgb(54, 162, 235)',
+    lineTension: 0.5
+  });
+  
   const data = {
-    labels,
-    datasets: datas,
-  };
-  const options = {
-    scales: {
-      y: {
-        //max: 800,
-        title: {display: true, text: `${eenheid}`, font: {size: 25, weight: 600, }},
-        ticks: {
-          font: {
-            size: 25,
-            family: 'vazir',
-            weight: 600,
-          }
-        }
-
-      },
-
-    },
-    plugins: {
-      annotation: {
-        annotations: [
-          {
-            type: "line",
-            mode: "horizontal",
-            scaleID: "y-axis-0",
-            yMin: doelwaardes,
-            yMax: doelwaardes,
-            borderColor: "orange",
-            borderWidth: 5,
-            label: {
-              enabled: true,
-              content: `${doelwaardes}`,
-              font: {
-                size: 25,
-              }
-            },
-          },
-        ],
-      },
-      legend: {
-        display: true,
-        labels: {
-          font: {
-            size: 25,
-            weight: 600,
-          },
-        }
-      },
-      zoom: {
-        zoom: {
-          wheel: {
-            enabled: false, // SET SCROLL ZOOM TO TRUE
-          },
-          mode: "xy",
-          speed: 100,
-        },
-        pan: {
-          enabled: true,
-          mode: "xy",
-          speed: 100,
-        },
-      },
-    },
+    //labels: alldata.map(e => {return ""}),
+    labels: ["", "", ""],
+    datasets: datas
   };
 
   return (
-    x && dataD &&
+    x && alldata && dataD && <>
     <div className={styles["barchart"]}>
-      <Bar data={data} options={options} />
+      {/*<Bar data={data}  />*/}
+      {<Bar type='bar' data={data} />}
+
     </div>
+    </>
+    
   );
 };
 
